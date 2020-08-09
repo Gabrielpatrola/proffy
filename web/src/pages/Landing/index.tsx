ad5226f9be59a6c63ed16dc22319eee924f6bd07
Gabriel Augusto Almeida <almeidagabrielaugusto@gmail.com>
2020-08-09 15:10:22 -0400

    estilos

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import logoImg from '../../assets/images/logo.svg';
-import landingImg from '../../assets/images/landing.svg';
+import logoImg from "../../assets/images/logo.svg";
+import landingImg from "../../assets/images/landing.svg";
 
-import studyIcon from '../../assets/images/icons/study.svg';
-import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
-import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+import studyIcon from "../../assets/images/icons/study.svg";
+import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
+import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 
-import './styles.css';
-import TeacherForm from '../TeacherForm';
+import "./styles.css";
 
-function LandingPage() {
+function Landing() {
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
         <div className="logo-container">
           <img src={logoImg} alt="Proffy" />
-          <h2>Sua plataforma de estudos online.</h2>
+          <h2>Sua plataforma de estudos online</h2>
         </div>
 
         <img
@@ -32,17 +31,18 @@ function LandingPage() {
             Estudar
           </Link>
           <Link to="/give-classes" className="give-classes">
-            <img src={giveClassesIcon} alt="Dar aulas" />
+            <img src={giveClassesIcon} alt="Estudar" />
             Dar aulas
           </Link>
         </div>
 
-        <span className="total-connections">
-          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
+        <span className="total-conections">
+          Total de 200 conexões já realizadas
+          <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
     </div>
   );
 }
 
-export default LandingPage;
+export default Landing;
